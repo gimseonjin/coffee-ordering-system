280ee476f31f67242029bf65b4ee9f5d546be353
gimseonjin616 <66009926+gimseonjin@users.noreply.github.com>
2024-02-08 10:39:49 +0900

    Feat: Integrate MenuService into MenuController and add unit tests for service

--- src/menu/menu.controller.ts
+++ src/menu/menu.controller.ts
@@ -1,13 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
+import { MenuService } from './menu.service';
 
 @Controller('menu')
 export class MenuController {
+  constructor(private menuSvc: MenuService) {}
+
   @Get()
   getCoffeMenu() {
-    return [
-      { menuId: 1, name: 'latte', price: 1000 },
-      { menuId: 2, name: 'americano', price: 2000 },
-    ];
+    return this.menuSvc.getCoffeMenu()
   }
 
   @Get('/popular')
